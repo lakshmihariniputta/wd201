5005bd00ab1252805c1e8fd4e41c143ef80f7438
Lakshmi harini Putta <116016000+lakshmihariniputta@users.noreply.github.com>
2023-09-13 21:40:32 +0530

    Create listTodos.js

---
+++ todo-cli/listTodos.js
@@ -0,0 +1,12 @@
+//  listTodos.js
+const db = require("./models/index");
+const listTodo = async () => {
+  try {
+    await db.Todo.showList();
+  } catch (error) {
+    console.error(error);
+  }
+};
+(async () => {
+  await listTodo();
+})();
